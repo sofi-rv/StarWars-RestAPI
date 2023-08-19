e625593c3e11d6f53d1d856f91092cd8ef894b38
Antonio Martínez <antonio_martinez88@hotmail.com>
2023-08-19 02:08:58 +0000

    updtaed

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,85 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    search = People.query.all()    
+    search_serialize = list(map(lambda x: x.serialize(), search)) # search.map((item)=>{item.serialize()})
+    print("valor de search_serialize ", search_serialize)
+    
+    return jsonify(search_serialize), 200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_people_id(id):
+    try:
+        search = People.query.get(id)   
+        search_serialize = search.serialize()
+        print("valor de search_serialize ", search_serialize)    
+
+        return jsonify(search_serialize), 200
+
+    except Exception as error:
+        print(error)
+        return jsonify({"message":str(error)}), 500
+
+@app.route('/people', methods=['POST'])
+def add_people():
+    try:
+        body = request.get_json()
+        
+        new_register = People(
+            name= body["name"],
+            eye_color= body["eye_color"],
+            hair_color= body["hair_color"],
+            height= body["height"],
+            age= body["age"]
+        )
+
+        db.session.add(new_register)
+        db.session.commit()
+
+        print("body es: ", body)
+
+        return jsonify({"message":"El personaje se agregó"}), 200
+    except Exception as error:
+        print(error)
+        return jsonify({"message":str(error)}), 500
+
+@app.route('/people/<int:id>', methods=['PUT'])
+def edit_people_id(id):
+    try:
+        body = request.get_json()
+        search = People.query.get(id)   
+
+        search.name = body["name"],
+        search.eye_color = body["eye_color"],
+        search.hair_color =  body["hair_color"],
+        search.height =  body["height"],
+        search.age = body["age"]
+        db.session.commit()           
+
+        return jsonify({"message":"se editó correctamente"}), 200
+
+    except Exception as error:
+        print(error)
+        return jsonify({"message":str(error)}), 500
+
+    
+@app.route('/people/<int:id>', methods=['DELETE'])
+def delete_people_id(id):
+    try:
+        
+        search = People.query.get(id)   
+        db.session.delete(search)
+        db.session.commit()           
+
+        return jsonify({"message":"se eliminó correctamente"}), 200
+
+    except Exception as error:
+        print(error)
+        return jsonify({"message":str(error)}), 500
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +18,24 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(30), unique=True, nullable=False)
+    eye_color = db.Column(db.String(30), unique=False, nullable=True)
+    hair_color = db.Column(db.String(30), unique=False, nullable=True)
+    height = db.Column(db.Float, unique=False, nullable=False)
+    age = db.Column(db.Integer, unique=False, nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "eye_color":self.eye_color,
+            "hair_color": self.hair_color,
+            "height": self.height,
+            "age": self.age
+        }
+
+
